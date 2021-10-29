3950f316ddb894bfd5a95de11d7219dd760fa964
Sérgio Raphael A. Sampaio <67436514+Svetrar@users.noreply.github.com>
2021-10-28 22:39:08 -0300

    Update ex13_home.php

--- ex13_home.php
+++ ex13_home.php
@@ -1,17 +1,14 @@
 <!DOCTYPE html>
 <html>
-<head>
-</head>
-<body>
-<h1>3DAW</h1>
-<br>
-	<a href="ex13_inserirAluno.php">Inserir Aluno</a><br>
-	<a href="ex13_alterarAluno.php">Alterar Aluno</a><br>
-	<a href="ex13_listarAlunos.php">Listar Alunos</a><br>
-	<a href="ex13_excluirAluno.php">Excluir Aluno</a><br>
-	<a href="ex13_detalheAluno.php">Detalhe de Aluno</a><br>
-<br>
-</body>
+	<head>
+	</head>
+	<body>
+	<h1>3DAW</h1>
+	<br>
+		<a href="ex13_inserirAluno.php">Inserir</a><br>
+		<a href="ex13_alterarAluno.php">Alterar</a><br>
+		<a href="ex13_listarAlunos.php">Listar</a><br>
+		<a href="ex13_excluirArquivo.php">Excluir</a><br>
+	<br>
+	</body>
 </html>
-<?php
-?>
